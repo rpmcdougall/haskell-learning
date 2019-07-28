3b00aceb59ec2897a1acd3926099d690d304330b
Ryan McDougall <rp.mcdougall@gmail.com>
2019-07-27 21:05:40 -0500

    exercises up through perfects chap5

--- src/chap5/Exercises.hs
+++ src/chap5/Exercises.hs
@@ -13,6 +13,18 @@ square' n = [(x, y) | (x, y) <- grid n n, x /= y]
 replicate :: Int -> a -> [a]
 replicate n s = [ s | _ <- [0..n]]
 
+pyths :: Int -> [(Int, Int, Int)]
+pyths n = [(x, y, z) | x <- [1..n],
+                       y <- [1..n],
+                       z <- [1..n],
+                       x^2 + y^2 == z^2]
+
+perfects :: Int -> [Int]
+perfects n = [ x | x <- [1..n], sum (factors x) - x == x]
 
 
 
+-- Helper Functions from previous sections --
+
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
