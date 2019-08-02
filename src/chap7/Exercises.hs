cb74e02bd25387a334b88c560b2afda4e27f345e
RyanMcDougall <ryan.mcdougall@target.com>
2019-08-02 11:12:34 -0500

    exercise progress, need to fix drop/takeWhile to do all cases/empty lists

--- src/chap7/Exercises.hs
+++ src/chap7/Exercises.hs
@@ -6,3 +6,16 @@ module Exercises where
 -- exerciseMapFilter f p xs = map f (filter p xs)
 
 
+-- Define higher order functions from prelude
+-- all' :: (a-> Bool) -> [Bool] -> Bool
+all' p = and . map p 
+
+-- any' :: (a -> Bool) -> [Bool] -> Bool
+any' p = or . map p
+
+
+takeWhile :: (a -> Bool)  -> [a] -> [a]
+takeWhile p xs = [ x | x <- filter p xs]
+
+dropWhile :: (a -> Bool) -> [a] -> [a]
+dropWhile p xs = [x | x <- filter (not . p) xs]
